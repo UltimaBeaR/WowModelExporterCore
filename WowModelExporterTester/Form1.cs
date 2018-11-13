5d3898d7ddaf2097dbd1b3aa40001dd544ae4be5
UltimaBear <thebear@yandex.ru>
2018-11-13 23:44:49 +0300

    экспорт сцены с 1ой нодой. в блендере норм открывается

--- WowModelExporterTester/Form1.cs
+++ WowModelExporterTester/Form1.cs
@@ -19,29 +19,29 @@ namespace WowModelExporterTester
 
             var wowObject = exporter.LoadCharacter(WhRace.HUMAN, WhGender.MALE, new string[]
             {
-                // шлем
-                "161600",
-                // плечи
-                "161621",
-                // плащ
-                "163355",
-                // чест
-                "161602",
-                // брасы
-                "161629",
-                // руки
-                "161610",
-                // пояс
-                "161624",
-                // ноги
-                "161616",
-                // ступни
-                "161605"
+                //// шлем
+                //"161600",
+                //// плечи
+                //"161621",
+                //// плащ
+                //"163355",
+                //// чест
+                //"161602",
+                //// брасы
+                //"161629",
+                //// руки
+                //"161610",
+                //// пояс
+                //"161624",
+                //// ноги
+                //"161616",
+                //// ступни
+                //"161605"
             });
 
-            var test = new Class1();
+            var test = new Exporter();
 
-            textBox1.Text = test.GetStuff();
+            textBox1.Text = test.ExportWowObject(wowObject, "tessst.fbx").ToString();
         }
     }
 }
