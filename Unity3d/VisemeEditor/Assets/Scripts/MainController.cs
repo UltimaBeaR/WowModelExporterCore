aa0369d37ccbbdfe79dda83a2c8d484db1dd41e0
UltimaBear <thebear@yandex.ru>
2018-11-29 23:38:58 +0300

    Начальная настройка. Добавил готовый ассет RuntimeGizmo

--- Unity3d/VisemeEditor/Assets/Scripts/MainController.cs
+++ Unity3d/VisemeEditor/Assets/Scripts/MainController.cs
@@ -1,10 +1,27 @@
-﻿using UnityEngine;
+﻿using RuntimeGizmos;
+using UnityEngine;
 using WowModelExporterUnityPlugin;
 
 public class MainController : MonoBehaviour
 {
-    void Start()
+    public void LoadCharacterButtonClickHandler()
     {
-        var character = new CharacterBuilder().Build(WowheadModelLoader.WhRace.HUMAN, WowheadModelLoader.WhGender.MALE);
+        if (_characterGo == null)
+        {
+            _characterGo = new CharacterBuilder().Build(WowheadModelLoader.WhRace.HUMAN, WowheadModelLoader.WhGender.MALE);
+
+            var jaw = GameObject.Find("jaw");
+            var collider = jaw.AddComponent<SphereCollider>();
+            collider.radius = 0.1f;
+            
+        }
     }
+
+    public void TranslateRotateButtonClickHandler()
+    {
+        var gizmo = Camera.main.GetComponent<TransformGizmo>();
+        gizmo.type = gizmo.type == TransformType.Move ? TransformType.Rotate : TransformType.Move;
+    }
+
+    private GameObject _characterGo { get; set; }
 }
