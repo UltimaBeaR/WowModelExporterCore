2b003a4b3b97ed9cff5806f485efcd1a1b36c077
UltimaBear <thebear@yandex.ru>
2018-12-13 23:18:17 +0300

    Материалы при экспорте в fbx теперь создаются уникальные. Имена материалов тоже. Это исправило глюк с embedded материалами в 2017 юнити.

--- WowModelExporterCore/WowMaterial.cs
+++ WowModelExporterCore/WowMaterial.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing;
-using WowheadModelLoader;
+﻿using WowheadModelLoader;
 
 namespace WowModelExporterCore
 {
@@ -21,6 +20,56 @@ namespace WowModelExporterCore
         public bool BothSides { get; set; }
 
         public MaterialType Type { get; set; }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var objWowMaterial = obj as WowMaterial;
+
+            if (objWowMaterial == null)
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            // MainImage не учитываем потому что он сейчас всегда равен одной из 4х image-ей
+
+            if (!Equals(Image1, objWowMaterial.Image1))
+                return false;
+            if (!Equals(Image2, objWowMaterial.Image2))
+                return false;
+            if (!Equals(Image3, objWowMaterial.Image3))
+                return false;
+            if (!Equals(Image4, objWowMaterial.Image4))
+                return false;
+
+            if (!Equals(BothSides, objWowMaterial.BothSides))
+                return false;
+
+            if (!Equals(Type, objWowMaterial.Type))
+                return false;
+
+            // ToDo: когда появятся еще поля (свойства материала) - добавить их сюда тоже
+
+            return true;
+        }
+
+        public string GetUniqueName()
+        {
+            var texturesHash = string.Format("{0:X}",
+                (
+                    (Image1?.Hash ?? "null") +
+                    (Image2?.Hash ?? "null") +
+                    (Image3?.Hash ?? "null") +
+                    (Image4?.Hash ?? "null")
+                ).GetHashCode());
+
+            return Type.ToString() + "_" + (BothSides ? "both" : "cull") + "_" + texturesHash;
+        }
 
         public enum MaterialType
         {
